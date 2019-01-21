cdc030dab794747d41caf452dcd179971cbec3b8
Yong Hao TENG <yonghao.teng@gmail.com>
2019-01-21 18:43:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@
 ### ./get-reposense.py --tag v1.6.1            # Gets a specific version
 ### ./get-reposense.py --release --overwrite   # Overwrite RepoSense.jar, if exists, with the latest release
 
-./get-reposense.py --release
+./get-reposense.py --master --overwrite
 
 # Executes RepoSense
 # Do not change the default output folder name (reposense-report)
 ## Examples of other valid options; For more, please view the user guide
 ### java -jar RepoSense.jar -repos https://github.com/reposense/RepoSense.git
 
-java -jar RepoSense.jar -config ./configs
+java -jar RepoSense.jar -repos https://github.com/reposense/RepoSense.git https://github.com/MarkBind/markbind.git 
