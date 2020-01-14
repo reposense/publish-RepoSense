430eb072c0931fa12ee8497b1e6a35e8b6855ff9
James Pang <jamespang@u.nus.edu>
2020-01-14 22:42:44 +0800

    Update since date

--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@
 ## Examples of other valid options; For more, please view the user guide
 ### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
 
-java -jar RepoSense.jar --config ./configs
+java -jar RepoSense.jar --config ./configs --since d1
