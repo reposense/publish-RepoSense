064f328d27db835126d8d161073e395e2e57e548
sopa301 <96387349+sopa301@users.noreply.github.com>
2024-12-23 18:16:58 +0800

    Add new configs

--- configs/github.sh
+++ configs/github.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-REPO_SLUG=$GITHUB_REPOSITORY
-COMMIT=$GITHUB_SHA
-TOKEN_NAME="ACCESS_TOKEN"
-DEPLOY_KEY_NAME="DEPLOY_KEY"
-ENV_NAME="GitHub secrets"
-GIT_USERNAME="Deployment Bot (GitHub)"
-GIT_EMAIL="deploy@github.com"
-REPO_NAME=$(basename "$GITHUB_REPOSITORY")
-VITE_BASE_DIR="/$REPO_NAME/"
+# REPO_SLUG=$GITHUB_REPOSITORY
+# COMMIT=$GITHUB_SHA
+# TOKEN_NAME="ACCESS_TOKEN"
+# DEPLOY_KEY_NAME="DEPLOY_KEY"
+# ENV_NAME="GitHub secrets"
+# GIT_USERNAME="Deployment Bot (GitHub)"
+# GIT_EMAIL="deploy@github.com"
+
+if [ -z "$GITHUB_REPOSITORY" ]; then
+  VITE_BASE_DIR=  # Set to undefined (empty)
+else
+  REPO_NAME=$(basename "$GITHUB_REPOSITORY")
+  VITE_BASE_DIR="/$REPO_NAME/"
+fi
