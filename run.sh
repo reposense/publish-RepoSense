acb9b6ac79f089e8adbf856653c4d769167daccc
sopa301 <96387349+sopa301@users.noreply.github.com>
2024-12-23 23:43:17 +0800

    Revert unnecessary changes

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@
 ### ./get-reposense.py --commit abc123         # Gets a specific commit
 ### ./get-reposense.py --release --overwrite   # Overwrite RepoSense.jar, if exists, with the latest release
 
-./get-reposense.py --master
+./get-reposense.py --release
 
 # Executes RepoSense
 # Do not change the default output folder name (reposense-report)
