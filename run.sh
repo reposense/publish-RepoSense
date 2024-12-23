556125dcc50cd852995b20bc4ed501cbd6e44780
sopa301 <96387349+sopa301@users.noreply.github.com>
2024-12-23 15:24:47 +0800

    Revert to buggy commit

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@
 ### ./get-reposense.py --commit abc123         # Gets a specific commit
 ### ./get-reposense.py --release --overwrite   # Overwrite RepoSense.jar, if exists, with the latest release
 
-./get-reposense.py --master
+./get-reposense.py --commit f4cad78c41007171b1f1a9f4e61baa8cfe71c372
 
 # Executes RepoSense
 # Do not change the default output folder name (reposense-report)
