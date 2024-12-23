56fb5993fec5da5c46cc150169a3fcef64e6f9ae
sopa301 <96387349+sopa301@users.noreply.github.com>
2024-12-23 23:26:12 +0800

    Revert to default reposense retrieval

--- configs/github.sh
+++ configs/github.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# REPO_SLUG=$GITHUB_REPOSITORY
-# COMMIT=$GITHUB_SHA
-# TOKEN_NAME="ACCESS_TOKEN"
-# DEPLOY_KEY_NAME="DEPLOY_KEY"
-# ENV_NAME="GitHub secrets"
-# GIT_USERNAME="Deployment Bot (GitHub)"
-# GIT_EMAIL="deploy@github.com"
-
-if [ -n "$GITHUB_REPOSITORY" ]; then
-  REPO_NAME=$(basename "$GITHUB_REPOSITORY")
-  export VITE_BASE_DIR="/$REPO_NAME/"
-fi
-
-echo "VITE_BASE_DIR:$VITE_BASE_DIR"
+REPO_SLUG=$GITHUB_REPOSITORY
+COMMIT=$GITHUB_SHA
+TOKEN_NAME="ACCESS_TOKEN"
+DEPLOY_KEY_NAME="DEPLOY_KEY"
+ENV_NAME="GitHub secrets"
+GIT_USERNAME="Deployment Bot (GitHub)"
+GIT_EMAIL="deploy@github.com"
