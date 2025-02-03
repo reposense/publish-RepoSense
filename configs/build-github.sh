782edc129fcfa925a142f1ef283b5e5f3e09cfa4
sopa301 <96387349+sopa301@users.noreply.github.com>
2025-02-03 15:39:36 +0800

    Merge logic into main yml files

--- configs/build-github.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -n "$GITHUB_REPOSITORY" ]; then
-  REPO_NAME=$(basename "$GITHUB_REPOSITORY")
-  export VITE_BASE_DIR="/$REPO_NAME/"
-fi
-
-echo "VITE_BASE_DIR:$VITE_BASE_DIR"
--- configs/build-travis.sh
+++
@@ -1,6 +0,0 @@
-if [ -n "$TRAVIS_REPO_SLUG" ]; then
-  REPO_NAME=$(basename "$TRAVIS_REPO_SLUG")
-  export VITE_BASE_DIR="/$REPO_NAME/"
-fi
-
-echo "VITE_BASE_DIR:$VITE_BASE_DIR"
